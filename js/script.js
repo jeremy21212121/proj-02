90dbd0cf0fde08290bce1b30b97a29582d1448b0
Jeremy P <jeremy21@gmail.com>
2015-10-01 14:54:38 -0700

    first

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+     //adaptiveHeight: true,
+    // mode: 'fade',
+     auto: 'true',
+     pager: false,
+     controls: false,
+     slideWidth: 360,
+     minSlides: 1,
+     maxSlides: 3,
+     slideMargin: 0,
+     pause: 0,
+    // easing: 'linear',
+     speed: 2000,
+     ticker: true
+
+  });
+});
