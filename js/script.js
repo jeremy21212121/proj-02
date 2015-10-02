b87c1e1ea9f9e490f15db36450d4855150b54a32
Jeremy P <jeremy21@gmail.com>
2015-10-01 17:31:46 -0700

    removed un needed folder

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider({
-     //adaptiveHeight: true,
+     adaptiveHeight: true,
     // mode: 'fade',
      auto: 'true',
      pager: false,
      controls: false,
-     slideWidth: 360,
+     slideWidth: 384,
      minSlides: 1,
      maxSlides: 3,
      slideMargin: 0,
-     pause: 0,
-    // easing: 'linear',
-     speed: 2000,
-     ticker: true
+     pause: 100,
+    easing: 'linear',
+     speed: 2000
+    //  ticker: true
 
   });
 });
